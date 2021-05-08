4c05127d6b9b5f57206772e1075fdd011a0d3ada
Chr1sM1 <mzs0119@qq.com>
2021-05-08 16:35:52 +0800

    HelloServlet

---
+++ servlet01/src/main/java/com/sizhe/servlet/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.sizhe.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @ClassName HelloServlet
+ * @Description TODO
+ * @Author Chris
+ * @Date 2021/5/8
+ **/
+public class HelloServlet extends HttpServlet {
+    //由于get或者post只是请求实现的不同的方式，可以相互调用，业务逻辑都一样：
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        //OutputStream outputStream = resp.getOutputStream();
+        PrintWriter writer = resp.getWriter();//响应流
+        writer.println("Hello,servlet");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
